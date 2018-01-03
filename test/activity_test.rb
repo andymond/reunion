10040137ff3848a027b8a3d5197ae08391a594f9
Andrew Dymond <andy.dymond@gmail.com>
2018-01-03 09:36:37 -0700

    Update activity & test files through iteration 2

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,84 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_has_name
+    activity = Activity.new("hiking")
+
+    assert_equal "hiking", activity.name
+  end
+
+  def test_activity_begins_with_no_participants
+    activity = Activity.new("hiking")
+
+    assert activity.participants.empty?
+  end
+
+  def test_participants_can_be_added
+    activity = Activity.new("hiking")
+
+    activity.add_participant("Pam", 10.99)
+
+    assert_equal ["Pam"], activity.participants.keys
+    assert_equal [10.99], activity.participants.values
+  end
+
+  def test_total_cost_of_activity_can_be_calculated
+    activity = Activity.new("hiking")
+
+    activity.add_participant("Pam", 1)
+    activity.add_participant("Jim", 2)
+    activity.add_participant("Michael", 3)
+    activity.add_participant("Dwight", 4)
+
+    assert_equal 10, activity.total_cost
+  end
+
+  def test_split_cost_splits_total_cost_of_activity
+    activity = Activity.new("hiking")
+
+    activity.add_participant("Pam", 1)
+    activity.add_participant("Jim", 2)
+    activity.add_participant("Michael", 3)
+    activity.add_participant("Dwight", 4)
+
+    assert_equal 2.5, activity.split_cost
+  end
+
+  def test_amount_owed_returns_amount_owed_by_participant
+    activity = Activity.new("hiking")
+
+    activity.add_participant("Pam", 1)
+    activity.add_participant("Jim", 2)
+    activity.add_participant("Michael", 3)
+    activity.add_participant("Dwight", 4)
+
+    assert_equal "Pam owes 1.5 dollars", activity.amount_owed("Pam")
+    assert_equal "Jim owes 0.5 dollars", activity.amount_owed("Jim")
+  end
+
+  def test_amount_owed_returns_amount_owed_to_participant
+    activity = Activity.new("hiking")
+
+    activity.add_participant("Pam", 1)
+    activity.add_participant("Jim", 2)
+    activity.add_participant("Michael", 3)
+    activity.add_participant("Dwight", 4)
+
+    assert_equal "Michael is owed 0.5 dollars", activity.amount_owed("Michael")
+    assert_equal "Dwight is owed 1.5 dollars", activity.amount_owed("Dwight")
+  end
+
+  def test_amount_owed_returns_squared_up_if_nothing_owed
+    activity = Activity.new("hiking")
+
+    activity.add_participant("Pam", 1)
+    activity.add_participant("Jim", 1)
+    activity.add_participant("Michael", 1)
+    activity.add_participant("Dwight", 1)
+
+    assert_equal "Pam is all squared-up", activity.amount_owed("Pam")
+    assert_equal "Dwight is all squared-up", activity.amount_owed("Dwight")
+  end
+
 end
