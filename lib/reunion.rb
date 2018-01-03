3188e6aa176eea0df37e1992dc28f80196ce7cb6
Andrew Dymond <andy.dymond@gmail.com>
2018-01-03 10:25:13 -0700

    Complete split_costs method in reunion.rb

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,4 +1,5 @@
 require_relative "activity"
+require 'pry'
 
 class Reunion
 
@@ -12,6 +13,26 @@ class Reunion
 
   def add_activity(name)
     activities << Activity.new(name)
+  end
+
+  def activity_total_costs
+    activities.map do |activity|
+      activity.total_cost
+    end
+  end
+
+  def total_cost
+    activity_total_costs.sum
+  end
+
+  def split_costs
+    activity_total_costs.map.with_index do |total_cost, index|
+      total_cost/activities[index].participants.length
+    end
+  end
+
+  def total_cost_breakdown
+
   end
 
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -27,6 +27,81 @@ class ReunionTest < Minitest::Test
     assert_equal "hiking", first_activity.name
   end
 
+  def test_total_cost_of_reunion_can_be_calculated
+    reunion = Reunion.new("Tallahassee")
 
+    reunion.add_activity("hiking")
+    first_activity = reunion.activities[0]
+    first_activity.add_participant("Andy", 1)
+    first_activity.add_participant("Michael", 1)
+    first_activity.add_participant("Dwight", 1)
+
+    assert_equal 3, reunion.total_cost
+
+    reunion.add_activity("boatin'")
+    second_activity = reunion.activities[1]
+    second_activity.add_participant("Andy", 2)
+    second_activity.add_participant("Michael", 3)
+    second_activity.add_participant("Dwight", 4)
+
+    assert_equal 12, reunion.total_cost
+  end
+
+  def test_split_costs_returns_array_of_split_costs
+    reunion = Reunion.new("Tallahassee")
+
+    reunion.add_activity("hiking")
+    first_activity = reunion.activities[0]
+    first_activity.add_participant("Andy", 1)
+    first_activity.add_participant("Michael", 1)
+    first_activity.add_participant("Dwight", 1)
+
+    reunion.add_activity("boatin'")
+    second_activity = reunion.activities[1]
+    second_activity.add_participant("Andy", 2)
+    second_activity.add_participant("Michael", 3)
+    second_activity.add_participant("Dwight", 4)
+
+    assert_equal [1, 3], reunion.split_costs
+  end
+
+  def test_money_spent_returns_array_of_arrays_of_money_spent
+    reunion = Reunion.new("Tallahassee")
+
+    reunion.add_activity("hiking")
+    first_activity = reunion.activities[0]
+    first_activity.add_participant("Andy", 1)
+    first_activity.add_participant("Michael", 1)
+    first_activity.add_participant("Dwight", 1)
+
+    reunion.add_activity("boatin'")
+    second_activity = reunion.activities[1]
+    second_activity.add_participant("Andy", 2)
+    second_activity.add_participant("Michael", 3)
+    second_activity.add_participant("Dwight", 4)
+
+    assert_equal [[1, 1, 1], [2, 3, 4]], reunion.money_spent
+  end
+
+  def test_total_cost_breakdown_of_union_can_be_calculated
+    skip
+    reunion = Reunion.new("Tallahassee")
+
+    reunion.add_activity("hiking")
+    first_activity = reunion.activities[0]
+    first_activity.add_participant("Andy", 1)
+    first_activity.add_participant("Michael", 1)
+    first_activity.add_participant("Dwight", 1)
+
+    reunion.add_activity("boatin'")
+    second_activity = reunion.activities[1]
+    second_activity.add_participant("Andy", 2)
+    second_activity.add_participant("Michael", 3)
+    second_activity.add_participant("Dwight", 4)
+
+    breakdown = {"Andy" => 1, "Michael" => 0, "Dwight" => -1}
+
+    assert_equal breakdown, reunion.total_cost_breakdown
+  end
 
 end
